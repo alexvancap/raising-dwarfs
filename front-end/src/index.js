16eace7a07801a8e3ec76035e60f095b5739adc4
alexvancap <alexvancap@gmail.com>
2019-12-31 11:22:09 -0600

    fixed loading mainpage

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,7 @@
-LOGGED_IN_USER_ID = 13
+LOGGED_IN_USER_ID = 1
 ASSET_ROOT = "http://localhost:3000"
 CONTAINER = document.getElementById("container")
+MESSAGES = document.getElementById("messages")
 IMAGE_PATH = "./src/img"
 
 
@@ -8,20 +9,23 @@ IMAGE_PATH = "./src/img"
 //IDEA: CHARACTER ONLY MOVES WHEN HOVERING OVER It
 
 
-loadMain()
+document.addEventListener("DOMContentLoaded", () => {
+    loadMain()
+})
+
 function loadMain(){
-    document.addEventListener("DOMContentLoaded", () => {
         getCharactersByUserId(LOGGED_IN_USER_ID).then((characterList) => {
+            console.log(characterList)
             if(characterList.length >= 1){
-                //go to see all characters
-                //change character list to 1
+                console.log(CONTAINER)
+                CONTAINER.innerHTML = ""
+                MESSAGES.innerHTML = ""
             }else{
                 createAlert("#messages", "Please chose your character!", "success", "prepend")
                 addCharacter()
             }
             showMoney(LOGGED_IN_USER_ID)
         })
-    })
 }
 
 
